1ce5908182e7694f415325d35a42f3a3e97d57a0
aortegan <toni.47.ortega@gmail.com>
2015-02-09 14:20:14 +0100

    First commit

---
+++ .env.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return array(
+
+    'DB_NAME'       => 'themosis',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => 'root',
+    'DB_HOST'       => 'localhost:8889',
+    'WP_HOME'       => 'http://localhost:8888/themosis/test-themosis/htdocs',
+    'WP_SITEURL'    => 'http://localhost:8888/themosis/test-themosis/htdocs/cms'
+
+);
---
+++ config/environment.php
@@ -0,0 +1,11 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define your environments
+/*----------------------------------------------------*/
+return array(
+
+    'local'         => 'mini-de-serveur',
+    'production'    => 'your-production-hostname'
+
+);
